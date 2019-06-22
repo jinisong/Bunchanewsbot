ef62aca7d2df978b2054c277a250e33099b1c698
jinisong <50916261+jinisong@users.noreply.github.com>
2019-06-22 15:28:29 +0900

    Update Crawl.py

--- Crawl.py
+++ Crawl.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+
 
 #vnexpress에서 startup으로 검색한 가장 최근 기사의 url를 가져오는 코드_
 data = requests.get('https://e.vnexpress.net/search?q=startup&media_type=all&cate_code=&search_f=title,lead&date_format=all&latest=on')         # get 요청으로 html을 가져와라
@@ -25,3 +30,22 @@ article=soup.find('div',{'class':"col main-list-text-col"})
 link = article.find_all('a')
 url3=link[2].get('href')
 print(url3)
+
+
+
+my_token = '729010308:AAGj0mPUkmRaf19gr3LLP9Wj8tYGbn69_w0'
+newsbot = telegram.Bot(token = my_token)
+
+def get_message(bot, update) :
+    update.message.reply_text("뉴스를 보냅니다.")
+    update.message.reply_text(url1)
+    update.message.reply_text(url2)
+    update.message.reply_text(url3)
+
+updater = Updater(my_token)
+
+message_handler = MessageHandler(Filters.text, get_message)
+updater.dispatcher.add_handler(message_handler)
+
+updater.start_polling(timeout=3, clean=True)
+updater.idle()
