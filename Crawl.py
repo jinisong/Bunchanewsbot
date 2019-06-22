2adead9fab77ac2271421c6405d37cffff7118ea
jinisong <50916261+jinisong@users.noreply.github.com>
2019-06-22 17:50:56 +0900

    Update Crawl.py

--- Crawl.py
+++ Crawl.py
@@ -6,13 +6,18 @@ from pymongo import MongoClient
 client = MongoClient('localhost',27017)
 db = client.dbsparta
 
+db.article.drop()
+
+#분짜뉴스봇과 연결하기
+my_token = '729010308:AAGj0mPUkmRaf19gr3LLP9Wj8tYGbn69_w0'
+news_bot = telegram.Bot(token = my_token)
+
+#e27
 data = requests.get('https://e27.co/search/?s=vietnam')
 soup = BeautifulSoup(data.text, 'html.parser')
 
 url = soup.find('a',{'class':{'dark-anchor'}}).get('href')
-print(url)
 db.article.insert_one({'url': url})
-
 
 
 #vnexpress에서 startup으로 검색한 가장 최근 기사의 url를 가져오는 코드_
@@ -21,8 +26,6 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 url=soup.find('h4').a.get('href')
 db.article.insert_one({'url': url})
-
-
 
 #Deal st.asia 검색어 vietnam 크롤링
 data = requests.get('https://www.dealstreetasia.com/?s=vietnam')         # get 요청으로 html을 가져와라
@@ -42,14 +45,19 @@ link = article.find_all('a')
 url=link[2].get('href')
 db.article.insert_one({'url': url})
 
-my_token = '729010308:AAGj0mPUkmRaf19gr3LLP9Wj8tYGbn69_w0'
-newsbot = telegram.Bot(token = my_token)
 
+
+def morningsend():
+    articles = db.article.find({})
+    for article in articles:
+        news_bot.sendMessage(text=article['url'])
+
+#말하면 봇이 쏩니다.
 def get_message(bot, update) :
-    update.message.reply_text("네. 빵야빵야")
-    update.message.reply_text(url1)
-    update.message.reply_text(url2)
-    update.message.reply_text(url3)
+    update.message.reply_text("네. 쏩니다 빵야빵야")
+    articles = db.article.find({})
+    for article in articles:
+        update.message.reply_text(article['url'])
 
 updater = Updater(my_token)
 
@@ -59,10 +67,7 @@ updater.dispatcher.add_handler(message_handler)
 updater.start_polling(timeout=3, clean=True)
 updater.idle()
 
-#e27 검색어 vietnam 크롤링
-data = requests.get('https://e27.co/search/?s=vietnam')         # get 요청으로 html을 가져와라
-soup = BeautifulSoup(data.text, 'html.parser')
-
-article=soup.find('a',{'class':{'dark-anchor'}})
-url4=soup.find('h4').a.get('href')
-print(url4)
+def morningsend():
+    articles = db.article.find({})
+    for article in articles:
+        news_bot.sendMessage(text=article['url'])
