770d19d3af8cd25c4d3680a249dd0ff8e65a987c
grace <blueskygk@gmail.com>
2019-06-29 12:36:11 +0900

    Update Crawl.py

--- Crawl.py
+++ Crawl.py
@@ -11,7 +11,6 @@ url = soup.find('a',{'class':{'dark-anchor'}}).get('href')
 print(url)
 db.article.insert_one({'url': url})
 
-
 #vnexpress에서 startup으로 검색한 가장 최근 기사의 url를 가져오는 코드_
 data = requests.get('https://e.vnexpress.net/search?q=startup&media_type=all&cate_code=&search_f=title,lead&date_format=all&latest=on')         # get 요청으로 html을 가져와라
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -32,15 +31,9 @@ print(url2)
 data = requests.get('https://www.dealstreetasia.com/?s=vietnam+korea')         # get 요청으로 html을 가져와라
 soup = BeautifulSoup(data.text, 'html.parser')
 
+
 article=soup.find('div',{'class':"col main-list-text-col"})
 link = article.find_all('a')
 url3=link[2].get('href')
 print(url3)
 
-#e27 검색어 vietnam 크롤링
-data = requests.get('https://e27.co/search/?s=vietnam')         # get 요청으로 html을 가져와라
-soup = BeautifulSoup(data.text, 'html.parser')
-
-article=soup.find('a',{'class':{'dark-anchor'}})
-url4=soup.find('h4').a.get('href')
-print(url4)
